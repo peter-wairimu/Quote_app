c2ed67a48c54c69450a6cb8bac116e375baa3d58
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-08-21 12:16:54 +0300

    added quote form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FooterComponent } from './footer/footer.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
